28d25a33ca5863f2e447245f19cf95604316fa02
菅野友美 <100789081+tomomikanno@users.noreply.github.com>
2022-03-23 04:07:49 +0900

    Update scrollreveal.js

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -3,17 +3,21 @@ ScrollReveal().reveal('.animation', {
     duration: 1000,
     viewFactor: 0.5,
     origin: 'bottom',
+    mobile: false,
 
 });
 
 ScrollReveal().reveal('.animation01', {
     delay: 200,
+    mobile: false,
 });
 ScrollReveal().reveal('.animation02', {
     delay: 450,
+    mobile: false,
 });
 ScrollReveal().reveal('.animation03', {
     delay: 700,
+    mobile: false,
 });
 
 ScrollReveal().reveal('.animation04', {
@@ -21,21 +25,25 @@ ScrollReveal().reveal('.animation04', {
     duration: 1000,
     viewFactor: 0.1,
     origin: 'bottom',
+    mobile: false,
 
 });
 ScrollReveal().reveal('.animation05', {
     scale: 2,
     duration: 1000,
     delay: 200,
+    mobile: false,
 });
 
 ScrollReveal().reveal('.animation06', {
     scale: 1.5,
     duration: 1000,
     delay: 200,
+    mobile: false,
 });
 ScrollReveal().reveal('.animation07', {
     scale: 1.5,
     duration: 1000,
     delay: 300,
+    mobile: false,
 });
